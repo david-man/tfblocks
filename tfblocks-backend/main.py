e0f5746be454c670ab105ce8194e2aca098e8618
david-man <86388290+david-man@users.noreply.github.com>
2025-08-11 23:38:12 -0400

    update main to use os.environ

--- tfblocks-backend/main.py
+++ tfblocks-backend/main.py
@@ -123,4 +123,4 @@ if __name__ == '__main__':
         if(sys.argv[1] == '--dev'):
             app.run(host='localhost', port=8000, debug = False)
     else:
-        app.run(host = '0.0.0.0', port=4000)
+        app.run(host = '0.0.0.0', port=os.getenv('PORT', 4000))
